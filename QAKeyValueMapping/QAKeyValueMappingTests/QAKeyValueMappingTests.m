ebe4e20a0c03875307fa301762dd4552a6823728
Quentin ARNAULT <quentin.arnault@niji.fr>
2014-02-27 22:32:23 +0100

    project initialization

---
+++ QAKeyValueMapping/QAKeyValueMappingTests/QAKeyValueMappingTests.m
@@ -0,0 +1,34 @@
+//
+//  QAKeyValueMappingTests.m
+//  QAKeyValueMappingTests
+//
+//  Created by Quentin Arnault on 27/02/2014.
+//  Copyright (c) 2014 Quentin Arnault. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QAKeyValueMappingTests : XCTestCase
+
+@end
+
+@implementation QAKeyValueMappingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
